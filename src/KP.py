4d12b2874027592bc5d8742af6e4891ff03ae5ad
SHIYI-WAN <71445396+SHIYI-WAN@users.noreply.github.com>
2021-07-05 21:20:23 +0200

    feat:implement cost function

--- src/KP.py
+++ src/KP.py
@@ -14,3 +14,15 @@ class KP:
         self.value = pd.DataFrame(data=dataset).iloc[:, 1]
         self.max_weight = max_weight
         self.afa = afa
+        
+    def cost(self):
+        '''
+        cost function
+        '''
+        total_cost = self.value.sum()
+        total_weight = self.weight.sum()
+        if total_weight <= self.max_weight:
+            total_cost = total_cost
+        else:
+            total_cost = total_cost - self.afa * (total_weight - self.max_weight)
+        return total_cost

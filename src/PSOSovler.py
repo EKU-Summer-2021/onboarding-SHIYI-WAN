958c014d04534b96a7cca2abe6dd37d2134eeac5
SHIYI-WAN <71445396+SHIYI-WAN@users.noreply.github.com>
2021-07-05 22:50:33 +0200

    feat: implement solver method sovle KP

--- src/PSOSovler.py
+++ src/PSOSovler.py
@@ -13,3 +13,38 @@ class PSOSolver:
         self.dim = dim  # search dimension
         self.max_iter = max_iter  # Maximum number of iterations
         self.kp = KP(dataset=kpdataset, max_weight=kpmax_weight, afa=kpafa)
+    def solver(self):
+        X = np.zeros((self.num, self.dim))
+        V = np.zeros((self.num, self.dim))
+        p_best = np.zeros((self.num, self.dim), dtype=float)
+        g_best = np.zeros((1, self.dim), dtype=float)
+        p_bestfit = np.zeros(self.num)
+        g_bestfit = -1e15
+
+        for i in range(self.num):
+            X[i] = np.random.uniform(0, 5, [1, self.dim])
+            V[i] = np.random.uniform(0, 5, [1, self.dim])
+            p_best[i] = X[i]
+            p_bestfit[i] = self.kp.cost(X[i])
+            if p_bestfit[i] > g_bestfit:
+                g_bestfit = p_bestfit[i]
+                g_best = X[i]
+        fitness = []
+
+        for _ in range(self.max_iter):
+            for i in range(self.num):
+                temp = self.kp.cost(X[i])
+                if temp > p_bestfit[i]:
+                    p_bestfit[i] = temp
+                    p_best[i] = X[i]
+                    if p_bestfit[i] > g_bestfit:
+                        g_best = X[i]
+                        g_bestfit = p_bestfit[i]
+
+            for i in range(self.num):
+                V[i] = self.w * V[i] + self.c1 * self.r1 * (p_best[i] - X[i]) + self.c2 * self.r2 * (g_best - X[i])
+                X[i] = X[i] + V[i]
+
+            fitness.append(g_bestfit)
+
+        print(g_best, g_bestfit)

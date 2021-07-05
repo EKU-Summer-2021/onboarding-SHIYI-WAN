c41742d2f895e0feabcaaf69dde66c960ac540e5
SHIYI-WAN <71445396+SHIYI-WAN@users.noreply.github.com>
2021-07-05 17:49:19 +0200

    fix:module and function docstring

--- src/read_csv.py
+++ src/read_csv.py
@@ -1,18 +1,24 @@
-import pandas as pd
+'''
+read csv module with 3 functions.
+'''
 import urllib.request
-
-
+import pandas as pd
+'''
+fuction to fetch data.
+'''
 def fetch_data():
-    path = "https://github.com/EKU-Summer-2021/intelligent_system_data/blob/main/Intelligent%20System%20Data/KP/KP_10" \
-           ".csv "
+    path = "https://github.com/EKU-Summer-2021/intelligent_system_data/blob/main/" \
+    "Intelligent%20System%20Data/KP/KP_10.csv "
     urllib.request.urlretrieve(path, "10.csv")
-
-    
-    
+'''
+fuction to load data.
+'''
 def load_data():
     fetch_data()
     csv_path = "10.csv"
     return pd.read_csv(csv_path, names=["w", "v"])
-
+'''
+fuction to summary data.
+'''
 def summary_data():
     return load_data().describe()

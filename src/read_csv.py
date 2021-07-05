e3217f733997335971b9539d03b956804523fcfb
SHIYI-WAN <71445396+SHIYI-WAN@users.noreply.github.com>
2021-07-05 17:17:28 +0200

    feat: implement data reading function

--- src/read_csv.py
+++ src/read_csv.py
@@ -6,3 +6,10 @@ def fetch_data():
     path = "https://github.com/EKU-Summer-2021/intelligent_system_data/blob/main/Intelligent%20System%20Data/KP/KP_10" \
            ".csv "
     urllib.request.urlretrieve(path, "10.csv")
+
+    
+    
+def load_data():
+    fetch_data()
+    csv_path = "10.csv"
+    return pd.read_csv(csv_path, names=["w", "v"])

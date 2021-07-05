b770aa3275b5b9dbe039f1fadd71e19915c2f214
SHIYI-WAN <71445396+SHIYI-WAN@users.noreply.github.com>
2021-07-05 22:58:00 +0200

    fix: load_data method para

--- tests/read_csv.py
+++ tests/read_csv.py
@@ -7,5 +7,6 @@ from src.read_csv import load_data
 
 class ReadCsvTest(unittest.TestCase):
     def test_read_Csv(self):
-        data = load_data()
+        data = load_data('https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent'
+                        '%20System%20Data/CSP/CSP_360.csv',"360.csv")
         self.assertIsInstance(data, pandas.DataFrame)

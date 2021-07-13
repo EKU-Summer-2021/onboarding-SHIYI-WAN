6f6b6b931d728c62ac8f2891599d9691c2db37cc
SHIYI-WAN <71445396+SHIYI-WAN@users.noreply.github.com>
2021-07-13 15:31:50 +0200

    fix:error

--- tests/read_csv.py
+++ tests/read_csv.py
@@ -3,9 +3,7 @@ Example module for template project.
 Pylint will check code in the src directory only!
 '''
 import unittest
-
 import pandas as pd
-
 from src.read_csv import load_data
 
 
@@ -13,13 +11,14 @@ class ReadCsvTest(unittest.TestCase):
     '''
     read csv test class
     '''
+
     def test(self):
         '''
         test method
         '''
-        data = load_data('https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data'
-                         '/main/Intelligent'
-                         '%20System%20Data/CSP/CSP_360.csv')
+        data = load_data('https://raw.githubusercontent.com/EKU-Summer-2021/'
+                         'intelligent_system_data/'
+                         'main/Intelligent'
+                        '%20System%20Data/CSP/CSP_360.csv')
         dataframe = pd.DataFrame(data)
         self.assertIsInstance(dataframe, pd.DataFrame)
-

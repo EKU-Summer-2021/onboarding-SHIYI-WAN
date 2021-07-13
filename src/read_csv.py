5417dba3a5255c9eec1df2f41f48469459a7b514
SHIYI-WAN <71445396+SHIYI-WAN@users.noreply.github.com>
2021-07-13 15:20:53 +0200

    fix:style

--- src/read_csv.py
+++ src/read_csv.py
@@ -1,12 +1,10 @@
 '''
 read csv module with 3 functions.
 '''
-import urllib.request
 import pandas as pd
 
-def load_data(path,filename):
+def load_data(path):
     '''
     fuction to fetch data.
     '''
-    urllib.request.urlretrieve(path, filename)
-    return pd.read_csv(filename, names=["w", "v"])
+    return pd.read_csv(path)

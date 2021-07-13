ecd1c4eb77307fec3df5e0061ac0142fdba538e7
SHIYI-WAN <71445396+SHIYI-WAN@users.noreply.github.com>
2021-07-13 15:01:47 +0200

    fix:put all function into one

--- src/read_csv.py
+++ src/read_csv.py
@@ -4,21 +4,9 @@ read csv module with 3 functions.
 import urllib.request
 import pandas as pd
 
-def fetch_data(path,filename):
+def load_data(path,filename):
     '''
     fuction to fetch data.
     '''
     urllib.request.urlretrieve(path, filename)
-
-def load_data(path,filename):
-    '''
-    fuction to load data.
-    '''
-    fetch_data(path,filename)
     return pd.read_csv(filename, names=["w", "v"])
-
-def summary_data(path,filename):
-    '''
-    fuction to summary data.
-    '''
-    return load_data(path,filename).describe()

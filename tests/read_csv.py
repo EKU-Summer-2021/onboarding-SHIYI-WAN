c6abe5a2e52ad31a0c45ff12b93043b9e9a30d42
SHIYI-WAN <71445396+SHIYI-WAN@users.noreply.github.com>
2021-07-13 15:19:37 +0200

    fix: style

--- tests/read_csv.py
+++ tests/read_csv.py
@@ -1,12 +1,25 @@
+'''
+Example module for template project.
+Pylint will check code in the src directory only!
+'''
 import unittest
 
-import pandas
+import pandas as pd
 
 from src.read_csv import load_data
 
 
 class ReadCsvTest(unittest.TestCase):
-    def test_read_Csv(self):
-        data = load_data('https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent'
-                        '%20System%20Data/CSP/CSP_360.csv',"360.csv")
-        self.assertIsInstance(data, pandas.DataFrame)
+    '''
+    read csv test class
+    '''
+    def test(self):
+        '''
+        test method
+        '''
+        data = load_data('https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data'
+                         '/main/Intelligent'
+                         '%20System%20Data/CSP/CSP_360.csv')
+        dataframe = pd.DataFrame(data)
+        self.assertIsInstance(dataframe, pd.DataFrame)
+
